25cdb226571f7bf889bb7d4245c8561184de3cd9
Oláh Alex <Olah.Alex@stud.u-szeged.hu>
2021-11-30 23:49:54 +0100

    Add solver boilerplate

---
+++ AdventOfCode/Task/ITask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Task
+{
+    public interface ITask
+    {
+        string Result { get; set; }
+        void solve();
+    }
+}
---
+++ AdventOfCode/Util/TextReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode.Util
+{
+    public class TextReader : ITextReader
+    {
+        public string[] ReadLineByLine(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+}
